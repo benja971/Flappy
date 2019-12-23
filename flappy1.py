4cde7c9d44b9b135e8cc2bb0e74aae7ca2ce3681
NIDDAM <niddam.benjamin@orange.fr>
2019-12-22 20:28:57 -0400

    Tuyaux infini
    Bug: les tuyaux vont de plus en plus vite

--- flappy1.py
+++ flappy1.py
@@ -12,70 +12,64 @@ def images():
 	bank["perso"] = pygame.image.load("Dragon1.png").convert_alpha()
 
 	return bank
-	
+
+
 def monter_perso(y):
     y-=10
 
     return y
 
-def NewTuyeaux():
-	for i in range(1):
-		rect = bank["t1"].get_rect()
+
+def NewTuyeaux(rect, rect2):
+	for i in range(2):
 		rect.x = randint(300 , 400)
 		rect.y = randint(-100 , 100)
 		mesTuyaux.append(rect)
 
-		rect2 = bank["t2"].get_rect()
 		rect2.x = randint(300 , 400)
 		rect2.y = randint(300 , 500)
 		mesTuyaux2.append(rect2)
 
-		return mesTuyaux, mesTuyaux2, rect1, rect2
+	return mesTuyaux, mesTuyaux2
 
-horloge = pygame.time.Clock()
 
 fenetre = pygame.display.set_mode((400,600))
-fond = pygame.image.load("bg2-bell.jpg").convert()
+horloge = pygame.time.Clock()
 
 bank = images()
-
-mesTuyaux=[]
-mesTuyaux2=[]
+fond = pygame.image.load("bg2-bell.jpg").convert()
 
 rectPerso = bank["perso"].get_rect()
 rectPerso.x = 150
 rectPerso.y = 300- rectPerso.h
 
+rect = bank["t1"].get_rect()
+rect2 = bank["t2"].get_rect()
+
+mesTuyaux=[]
+mesTuyaux2=[]
 continuer = True
-i = 0 
-state = "menu"
+state = "jeu"
+i = 0
 
 pygame.key.set_repeat(4, 4)
 
 while continuer:
 	touches = pygame.key.get_pressed()
 	events = pygame.event.get()
-
-	if state == "menu":
-		if touches[pygame.K_ESCAPE]:
-			continuer = False
-
-		if touches[pygame.K_SPACE]:
-			state = "jeu "
-
-		fenetre.blit(fond, (0,0))
+	horloge.tick(60)	
 
 	if state == "jeu":
 		i += 1
-		horloge.tick(60)	
 		touch = False
-		rectPerso.y += 3
+		#rectPerso.y += 4
 
-		if i%100 == 0:
-			NewTuyeaux()		
+		if i%60 == 0:
+			NewTuyeaux(rect, rect2)		
 
 		if touches[pygame.K_ESCAPE]:
-			state = "menu"
+			#state = "menu"
+			continuer = 0 
 
 		if touches[pygame.K_SPACE]:
 			rectPerso.y -= 10
@@ -84,19 +78,22 @@ while continuer:
 		fenetre.blit(bank["perso"], rectPerso)
 
 		for r in mesTuyaux:
-			r.x -=5
+			r.x -=2
 			fenetre.blit(bank["t1"], r)
 
-			if rectPerso.colliderect(rect1):
-				touch = True
+			if rectPerso.colliderect(r):
+				#state = "menu"
+				print("Touché")
 
 		for t in mesTuyaux2:
-			t.x -=5
+			t.x -=2
 			fenetre.blit(bank["t2"], t)
 
-			if rectPerso.colliderect(rect2):
-				touch = True
+			if rectPerso.colliderect(t):
+				#state = "menu"
+				print("Touché")
+		print(horloge)
+				
 
 
 	pygame.display.flip()
-	
